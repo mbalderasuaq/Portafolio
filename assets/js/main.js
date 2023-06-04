d6a4bf7ae65cd245c1422e823943453214fbe141
mbalderasuaq <mbalderas08@alumnos.uaq.mx>
2023-06-03 22:24:01 -0600

    Se agrego el footer y adaptacion a otras medidas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,23 @@
+const imagen = document.getElementById("imagen-perfil");
+
 function openBtn(){
     document.getElementById("menu-mobil").style.width = "100%";
 }
 
 function closeBtn(){
     document.getElementById("menu-mobil").style.width = "0px";
+}
+
+function setDarkMode(){
+    setUserTheme("dark");
+    imagen.src = "assets/img/perfil.png";
+}
+
+function setLightMode(){
+    setUserTheme("light");
+    imagen.src = "assets/img/perfil2.png";
+}
+
+function setUserTheme(newTheme){
+    document.documentElement.setAttribute("data-theme", newTheme);
 }
