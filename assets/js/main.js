410a627814a9c8d21746bb9f97146c87b4b52660
mbalderasuaq <mbalderas08@alumnos.uaq.mx>
2023-06-03 22:41:22 -0600

    Se cambiaron los iconos y se cambiaron los nombres

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 const imagen = document.getElementById("imagen-perfil");
+const icono = document.getElementById("icono")
 
 function openBtn(){
     document.getElementById("menu-mobil").style.width = "100%";
@@ -10,12 +11,14 @@ function closeBtn(){
 
 function setDarkMode(){
     setUserTheme("dark");
-    imagen.src = "assets/img/perfil.png";
+    imagen.src = "assets/img/perfil-dark.png";
+    icono.href = "assets/ico/icono-dark.ico"
 }
 
 function setLightMode(){
     setUserTheme("light");
-    imagen.src = "assets/img/perfil2.png";
+    imagen.src = "assets/img/perfil-light.png";
+    icono.href = "assets/ico/icono-light.ico"
 }
 
 function setUserTheme(newTheme){
